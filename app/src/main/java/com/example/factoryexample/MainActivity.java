e1de74c2a46b9a557a792d06e955c6b653342326
Ali Yousefpoor <Ali@Mahdis-MBP.lan>
2020-06-29 13:52:09 +0430

    Add DialogAlert

--- app/src/main/java/com/example/factoryexample/MainActivity.java
+++ app/src/main/java/com/example/factoryexample/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.factoryexample;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button CBtn, RBtn, SBtn;
-    ShapeFactory shapeFactory = new ShapeFactory();
-    Shape cShape = shapeFactory.getShape("CIRCLE");
-    Shape rShape = shapeFactory.getShape("RECTANGLE");
-    Shape sSHAPE = shapeFactory.getShape("SQUARE");
+     ShapeFactory shapeFactory = new ShapeFactory();
+     Shape cShape = shapeFactory.getShape("CIRCLE");
+     Shape rShape = shapeFactory.getShape("RECTANGLE");
+     Shape sShape = shapeFactory.getShape("SQUARE");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +31,54 @@ public class MainActivity extends AppCompatActivity {
         CBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cShape.draw(getApplicationContext());
+                cShape.draw(MainActivity.this);
             }
         });
 
         RBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rShape.draw(getApplicationContext());
+                rShape.draw(MainActivity.this);
             }
         });
         SBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sSHAPE.draw(getApplicationContext());
+                sShape.draw(MainActivity.this);
             }
         });
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        {
+            builder.setMessage("Enter your message here !");
+            builder.setCancelable(true);
+            builder.setPositiveButton("Circle", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    cShape.draw(MainActivity.this);
+                }
+            });
+
+            builder.setNegativeButton("Rectangle", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    rShape.draw(MainActivity.this);
+                }
+            });
+
+            builder.setNeutralButton("Square", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    sShape.draw(MainActivity.this);
+                }
+            });
+
+
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
 
     }
+
+
 }
